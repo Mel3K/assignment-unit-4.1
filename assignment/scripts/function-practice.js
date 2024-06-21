8d48b92a317f5c90c38198a6f98eaa2825c3f40c
Meleah Roe <melroe19@gmail.com>
2024-06-21 15:58:40 -0500

    made hello your name function

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,11 +14,13 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
+let name = "Mel";
+
 function helloName(name) {
-  return 'Hello,${name}!';
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-console.log('test -should say "Hello, Joe!', helloName());
+console.log('test -should say "Hello, Joe!', helloName(name));
 
 
 // 3. Function to add two numbers together & return the result
